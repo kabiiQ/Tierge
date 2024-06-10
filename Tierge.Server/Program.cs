85f0c44ebf8653c31cfc44aef8a9bdb84cd3c293
kabii <jackyd.1018@live.com>
2024-06-09 22:00:49 -0500

    Initial templated project commit

---
+++ Tierge.Server/Program.cs
@@ -0,0 +1,42 @@
+using Tierge.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(TiergeApp.Frontend);
+    });
+});
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.UseCors();
+
+app.Run();
---
+++ tierge.client/src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const API = "https://localhost:32770/api";
